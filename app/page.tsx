350c577da6ec52ffdae251e690f35361157eb3f8
youknowme <ys1113457623@gmail.com>
2023-02-15 01:09:59 +0530

    sanity backend added

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 export default function HomePage(){
     return(
         <div>
-            <h1 className="text-4xl">Welcome to the Blog</h1>
-            <p>Some content</p>
+            <h1 className="text-4xl ">Welcome to the Blog</h1>
+            <p className="te">Some content</p>
         </div>
     )
 }
---
+++ bloggin-website-sanity/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'slju31y4',
+    dataset: 'production'
+  }
+})
