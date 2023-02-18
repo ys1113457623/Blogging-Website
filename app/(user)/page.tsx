029d05569f694823a0036afc1cd269d74097726f
youknowme <ys1113457623@gmail.com>
2023-02-18 20:22:45 +0530

    Page Started Previewing

--- app/(user)/page.tsx
+++ app/(user)/page.tsx
@@ -1,7 +1,28 @@
-export default function HomePage(){
+import { previewData } from 'next/headers'
+import {groq} from 'next-sanity';
+import { client } from '../../lib/sanity.client';
+import PreviewSuspense from '../../components/PreviewSuspense';
+import PreviewBlogList from '../../components/PreviewBlogList';
+import BlogList from '../../components/BlogList';
+
+const query = groq`
+*[ _type == "post"]{...,author->, categories[]->}|order(_createdAt desc)`
+
+export default async function HomePage(){
+    if(previewData()){
+        return (
+            <PreviewSuspense fallback={(
+                <div role="status">
+                    <p className='text-center text-ls animate-pulse text-[#F7AB0A]'>
+                        Loading Preview Data...
+                    </p>
+                </div>
+            )} ><PreviewBlogList query={query} /></PreviewSuspense>
+        )
+    }
+    const posts = await client.fetch(query);
+    // const posts = await client.fetch(query);
     return(
-        <div>
-            
-        </div>
+        <BlogList posts={posts} />
     );
 }
